4fe73779be8fa72e852bc8a4622a94ba1a00a650
rahulGarg003 <rahul.garg9460@gmail.com>
2023-04-11 17:13:56 +0530

    added application.py file

--- application.py
+++ application.py
@@ -1,1 +1,5 @@
-from src.logger import logging
+from src.pipelines.training_pipeline import TrainingPipeline
+
+if __name__ == '__main__':
+    pipeline = TrainingPipeline()
+    pipeline.initiate_training_pipeline()
