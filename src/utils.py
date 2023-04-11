fb96c524a2fd43ec8ce41fc65c707ba56ab4e3ac
rahulGarg003 <rahul.garg9460@gmail.com>
2023-04-11 13:37:31 +0530

    Added logic to save pkl file

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import os
 import sys
+import pickle
+
+from src.logger import logging
+from src.exception import CustomException
 
 def load_dataframe_from_csv(file_path: str) -> pd.DataFrame:
-    return pd.read_csv(file_path)
+    try:
+        return pd.read_csv(file_path)
+    except Exception as e:
+        raise CustomException(e, sys)
 
 def save_dataframe_to_csv(df: pd.DataFrame, file_path: str, index: bool, header: bool) -> None:
-    df.to_csv(file_path, index=index, header=header)
+    try:
+        df.to_csv(file_path, index=index, header=header)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def save_object_as_pkl(file_path: str, obj):
+    try:
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(
+                obj=obj,
+                file=file_obj
+            )
+    except Exception as e:
+        CustomException(e, sys)
